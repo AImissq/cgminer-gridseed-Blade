639e6be69ca54ab1e16cebd493036db6d4178a57
Evolvia <linux.dev@free.fr>
2014-05-06 09:44:54 +0200

    Versioning Modification Add automaking exec

---
+++ genexec.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+echo 'Configuring cgminer 3.7.3 GSD 1.1 ....\r\n'
+./configure --enable-scrypt --enable-gridseed --with-build-number=1.1
+echo 'Making cgminer 3.7.3 GSD 1.1 ....\r\n'
+make
+if test -f cgminer
+then
+echo 'Verifying cgminer Version .....\r\n'
+./cgminer --version
+echo 'All is right .... Good mining :o) \r\n'
+else
+echo 'Making error try again :o) \r\n'
+fi
